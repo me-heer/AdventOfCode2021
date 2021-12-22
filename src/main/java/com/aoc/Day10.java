a829117d71af70d4923e880ddfe98d7445bd5b9c
Mihir Joshi <mihir.joshi@senpiper.com>
2021-12-22 23:03:57 +0530

    Solved Day 10

--- src/main/java/com/aoc/Day10.java
+++ src/main/java/com/aoc/Day10.java
@@ -1,9 +1,7 @@
 package com.aoc;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day10 extends AdventOfCode{
     @Override
@@ -52,14 +50,17 @@ public class Day10 extends AdventOfCode{
 
 
         }
-        System.out.println(points);
 
+        ArrayList<Long> totalPoints = new ArrayList<>();
         for(String inputLine: inputLines) {
-
+            totalPoints.add(findExpectedBracketPoints(inputLine));
         }
+
+        List<Long> sortedTotalPoints = totalPoints.stream().sorted().collect(Collectors.toList());
+        System.out.println(sortedTotalPoints.get(sortedTotalPoints.size() / 2));
     }
 
-    private String findExpectedBracket(String inputLine) {
+    private Long findExpectedBracketPoints(String inputLine) {
         Stack<Character> stack = new Stack<>();
         for(Character c: inputLine.toCharArray())
         {
@@ -91,35 +92,17 @@ public class Day10 extends AdventOfCode{
             }
 
         }
+
+        long points = 0;
         while (!stack.isEmpty()) {
             Character c = stack.pop();
             switch (c) {
-                case '{':
-                case '[':
-                case '(':
-                case '<': stack.push(c); break;
-                case '}':
-                    Character matchCurly = stack.pop();
-                    if(matchCurly != '{')
-                        System.out.println("Error");
-                    break;
-                case ')':
-                    Character matchRound = stack.pop();
-                    if(matchRound != '(')
-                        System.out.println("Error");
-                    break;
-                case '>':
-                    Character matchAngled = stack.pop();
-                    if(matchAngled != '<')
-                        System.out.println("Error");
-                    break;
-                case ']':
-                    Character matchSquare = stack.pop();
-                    if(matchSquare != '[')
-                        System.out.println("Error");
-                    break;
+                case '(': points = (points * 5) + 1; break;
+                case '[': points = (points * 5) + 2; break;
+                case '{': points = (points * 5) + 3; break;
+                case '<': points = (points * 5) + 4; break;
             }
         }
-        return "";
+        return points;
     }
 }
